5579dbdb6055fceb0abcf45aaab83bd128080b66
Kim-arcos <kimarcos16@gmail.com>
2024-01-28 21:54:20 -0700

    working on tab 3 :{

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
+import "./App.css";
+import { useState } from "react";
 import ContactList from "./components/ContactList";
-import React, { useState } from "react";
-
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
+import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-        <ContactList contacts={contacts} />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
